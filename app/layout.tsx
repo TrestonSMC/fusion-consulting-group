456d6194dfb1da1405d9f2a6ce0ffe7670225617
Treston Slater <treston@slatermediacompany.com>
2026-04-28 23:52:58 -0700

    add clean centered footer

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import FuzionNav from "../src/components/FuzionNav";
+import FuzionFooter from "../src/components/FuzionFooter";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -44,6 +45,7 @@ export default function RootLayout({
       <body className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
         <FuzionNav />
         {children}
+        <FuzionFooter />
       </body>
     </html>
   );
